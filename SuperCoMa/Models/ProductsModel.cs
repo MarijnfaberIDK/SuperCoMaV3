f3c03fec94d6db768e3860e2b587030f21c1a01f
Marijn Faber <faber5@live.nl>
2019-05-22 11:13:40 +0200

    Add project files.

---
+++ SuperCoMa/Models/ProductsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperCoMa.Models
+{
+    public class ProductsModel
+    {
+        public int ProductID { get; set; }
+        public string EAN { get; set; }
+        public string Title { get; set; }
+        public string Brand { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string Image { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string SubSubCategory { get; set; }
+    }
+}
