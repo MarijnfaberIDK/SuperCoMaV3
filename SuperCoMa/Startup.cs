b5d075ed30c19341fa3717e5da6c35817ce0b6b9
Marijn Faber <faber5@live.nl>
2019-06-12 10:55:20 +0200

    Startup.cs Session start

--- SuperCoMa/Startup.cs
+++ SuperCoMa/Startup.cs
@@ -28,6 +28,14 @@ namespace SuperCoMa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromDays(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -48,6 +56,7 @@ namespace SuperCoMa
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
